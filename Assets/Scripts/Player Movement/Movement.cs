3a43f20c15844ae409bddf461597a0923fca15c3
unknown <c.steuns@gmail.com>
2019-08-02 23:55:04 +0200

    basic movement

---
+++ Assets/Scripts/Player Movement/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed = 1;
+
+    public void Move(Vector2 direction) {
+        var movement = direction * speed * Time.deltaTime;
+        rb.transform.Translate(movement);
+    }
+
+}
---
+++ Assets/Scripts/Player Movement/MovementController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    [SerializeField]
+    private Movement movement;
+
+    void Update() {
+        Vector2 input = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")
+        );
+
+        movement.Move(input);
+
+    }
+}
