6ad2ac22e17db43908ffcb65055620a24ed2a740
jvdwijk <joeyvdwijk@gmail.com>
2019-08-03 13:38:17 +0200

    Text Mesh Pro added, Character Info Added + fade

---
+++ Assets/Scripts/Possession/Citizen.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class Citizen : MonoBehaviour
+{
+    [SerializeField, Header("Character Info")]
+    private string characterName;
+    [SerializeField]
+    private int characterAge;
+
+    [SerializeField, Header("UI Objects")]
+    private TextMeshProUGUI nameText;
+    [SerializeField]
+    private TextMeshProUGUI ageText;
+    [SerializeField]
+    private TextMeshProUGUI convertedText;
+
+    [SerializeField, Header("Animation Objects")]
+    private Animator infoFadeAnimator;
+    [SerializeField]
+    private Canvas infoCanvas;
+    private const string animationBool = "InScreen";
+
+    private void OnMouseEnter(){
+        EnableInfo();
+    }
+
+    private void OnMouseExit(){
+        StartCoroutine(DisableInfoRoutine());
+    }
+
+    private void EnableInfo(){
+        print("yum");
+        infoCanvas.enabled = true;
+        infoFadeAnimator.SetBool(animationBool, true);
+
+        nameText.text = characterName;
+        ageText.text = "Age: " + characterAge.ToString();
+    }
+
+    private IEnumerator DisableInfoRoutine(){
+        infoFadeAnimator.SetBool(animationBool, false);
+        yield return new WaitForSeconds(0.5f);
+        infoCanvas.enabled = false;
+    }
+}
