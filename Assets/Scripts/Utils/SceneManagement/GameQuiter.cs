a1e77c204525408ec5cec4618c7d59df973e5ce4
jvdwijk <joeyvdwijk@gmail.com>
2019-08-04 14:01:56 +0200

    Buildsettings + Main menu and credits

---
+++ Assets/Scripts/Utils/SceneManagement/GameQuiter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PeppaSquad.Utils
+{
+    public class GameQuiter : MonoBehaviour
+    {
+        /// <summary>
+        /// Closes the game
+        /// </summary>
+        public void QuitGame(){
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/SceneManagement/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Reloads the current active scene
+/// </summary>
+public class SceneSwitcher : MonoBehaviour {
+
+    /// <summary>
+    /// Reloads the current active scene
+    /// </summary>
+    public void ChangeScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void VisitUrl(string link){
+        Application.OpenURL(link);
+    }
+}
